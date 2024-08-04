67ddff3b88729e7130e2cf02471fd6b223de0e63
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-08-04 14:33:55 +0000

    Deploying to release from @ dotcomrow/pulse-gcp-db@73c352fe16947b60ca315e1a10540fe6543fef1f 🚀

---
+++ bigquery.tf
@@ -0,0 +1,28 @@
+resource "google_bigquery_dataset" "db" {
+  dataset_id  = "${var.project_name}_dataset"
+  description = "Dataset for ${var.project_name} project"
+  location    = "US"
+
+  depends_on = [google_project_service.project_service]
+}
+
+module "schemas" {
+  source     = "./tables"
+  project_id = google_project.project.project_id
+  dataset_id = google_bigquery_dataset.db.dataset_id
+
+  depends_on = [google_bigquery_dataset.db]
+}
+
+resource "null_resource" "build_schema" {
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/build_graphql_schema.sh ${google_project.project.project_id} ${google_bigquery_dataset.db.dataset_id} ${var.bucket_name} ${var.r2_account_id} ${var.r2_access_key_id} ${var.r2_secret_access_key}"
+  }
+
+  depends_on = [module.schemas]
+}
