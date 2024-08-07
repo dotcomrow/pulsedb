7211e9a6e2549425271d3134b1604badf936ec8c
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-07 17:34:32 -0400

    ?
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- tables/user_info.tf
+++ tables/user_info.tf
@@ -1,9 +1,9 @@
 resource "google_bigquery_table" "user_info" {
-  dataset_id = var.dataset_id
-  table_id   = "user_info"
+  dataset_id          = var.dataset_id
+  table_id            = "user_info"
   deletion_protection = false
-  project                     = var.project_id
-  schema = <<EOF
+  project             = var.project_id
+  schema              = <<EOF
 [
   {
     "mode": "REQUIRED",
