f90c08b772d423e46ffc34b0580455a124e4c369
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-08-07 19:02:09 +0000

    Deploying to release from @ dotcomrow/pulsedb@703bd408bae2d72b9a3cb1d3de29fdf2ae69a7cb 🚀

---
+++ routines/get_row_id.tf
@@ -0,0 +1,12 @@
+resource "google_bigquery_routine" "get_row_id" {
+  dataset_id      = var.dataset_id
+  routine_id      = "get_row_id"
+  routine_type    = "PROCEDURE"
+  language        = "SQL"
+  
+  arguments {
+    name = "sequence_name"
+    data_type = "{\"typeKind\" :  \"STRING\"}"
+  } 
+  definition_body = templatefile("${path.module}/../templates/get_row_id.template", { dataset = var.dataset_id })
+}
