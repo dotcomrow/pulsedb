edd5c9e02283642e63a6692a555b702434470737
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-07 14:58:44 -0400

    fixing dataset id
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- routines/get_row_id.tf
+++ routines/get_row_id.tf
@@ -1,5 +1,5 @@
 resource "google_bigquery_routine" "get_row_id" {
-  dataset_id      = google_bigquery_dataset.main_dataset.dataset_id
+  dataset_id      = var.dataset_id
   routine_id      = "get_row_id"
   routine_type    = "PROCEDURE"
   language        = "SQL"
@@ -8,7 +8,5 @@ resource "google_bigquery_routine" "get_row_id" {
     name = "sequence_name"
     data_type = "{\"typeKind\" :  \"STRING\"}"
   } 
-  definition_body = templatefile("${path.module}/templates/get_row_id.template", { dataset = google_bigquery_dataset.main_dataset.dataset_id })
-
-  depends_on = [ google_bigquery_dataset.main_dataset ]
+  definition_body = templatefile("${path.module}/templates/get_row_id.template", { dataset = var.dataset_id })
 }
