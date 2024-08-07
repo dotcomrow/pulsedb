a4d1d8c07c8569a678f3201bc595afc6055303c9
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-08-07 19:00:48 +0000

    Deploying to release from @ dotcomrow/pulsedb@e40e21448021ff5f0bd65107471b771eff8b89e5 🚀

---
+++ bigquery.tf
@@ -0,0 +1,37 @@
+resource "google_bigquery_dataset" "db" {
+  dataset_id  = "${var.project_name}_dataset"
+  description = "Dataset for ${var.project_name} project"
+  location    = "US"
+  project = google_project.project.project_id
+
+  depends_on = [google_project_service.project_service]
+}
+
+module "schemas" {
+  source     = "./tables"
+  project_id = google_project.project.project_id
+  dataset_id = google_bigquery_dataset.db.dataset_id
+
+  depends_on = [google_bigquery_dataset.db]
+}
+
+module "functions" {
+  source     = "./routines"
+  project_id = google_project.project.project_id
+  dataset_id = google_bigquery_dataset.db.dataset_id
+
+  depends_on = [google_bigquery_dataset.db]
+}
+
+resource "null_resource" "build_schema" {
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/build_graphql_schema.sh ${google_project.project.project_id} ${google_bigquery_dataset.db.dataset_id} ${var.bucket_name} ${var.R2_account_id} ${var.R2_access_key_id} ${var.R2_secret_access_key}"
+  }
+
+  depends_on = [module.schemas]
+}
