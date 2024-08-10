94937edf18024e3027c6a11888d63baf255ec750
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-10 16:25:14 -0400

    bucket name
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,4 +1,4 @@
 resource "cloudflare_r2_bucket" "cloudflare-bucket" {
   account_id = var.cloudflare_account_id
-  name       = var.bucket_name
+  name       = "${var.bucket_name}=${var.DATASET_ENV}"
 }
