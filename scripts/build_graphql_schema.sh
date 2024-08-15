99729f59523567bfa66a386f253564d188c24068
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-08-15 12:25:52 +0000

    Deploying to release from @ dotcomrow/pulsedb@d71f51957bddac7931ea0fb09fde9c7af93d33ba 🚀

---
+++ scripts/build_graphql_schema.sh
@@ -0,0 +1,27 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install node
+
+cat > package.json <<EOF
+PKG_JSON_CONTENTS
+EOF
+
+npm install
+
+cat > schema_builder.js <<EOF
+JS_CONTENTS
+EOF
+
+
+
+node schema_builder.js
+
+cat <<EOF
+{
+  "res": "test"
+}
+EOF
